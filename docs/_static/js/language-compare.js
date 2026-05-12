764b5cbc7da14943009ca47a929f24398b27c941
David Eyal <davideyal@pop-os.localdomain>
2026-05-12 17:14:29 +0200

    fix: strip GitHub Pages base from original links

--- docs/_static/js/language-compare.js
+++ docs/_static/js/language-compare.js
@@ -1,8 +1,15 @@
 (function () {
   var upstreamOrigin = "https://oi-wiki.org";
+  var scriptPath = document.currentScript && document.currentScript.src
+    ? new URL(document.currentScript.src).pathname
+    : "/_static/js/language-compare.js";
+  var siteBasePath = scriptPath.replace(/_static\/js\/language-compare\.js(?:\?.*)?$/, "");
 
   function currentUpstreamUrl() {
     var path = window.location.pathname || "/";
+    if (siteBasePath !== "/" && path.indexOf(siteBasePath) === 0) {
+      path = path.slice(siteBasePath.length - 1);
+    }
     return upstreamOrigin + path + window.location.hash;
   }
 
